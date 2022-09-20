f4bc23f9d8703e92fc5e3e355e11c553e0098f4f
Daniel Walther <fractaled45@outlook.com>
2022-09-20 00:08:27 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="font-large">Test</h1>
+  <div class="m-32 border">
+    <EpicCalendar />
+  </div>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import EpicCalendar from "./components/EpicCalendar.vue";
+</script>
---
+++ src/components/EpicCalendar.vue
@@ -0,0 +1,17 @@
+<template>
+  <table class="m-auto">
+    <thead>
+      <tr>
+        <td>M</td>
+        <td>T</td>
+        <td>W</td>
+        <td>T</td>
+        <td>F</td>
+        <td>S</td>
+        <td>S</td>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script setup lang="ts"></script>
